d943689c4cfa0f3c078eedd3f1412d4f678f221d
springthree1442 <green.oliveade@gmail.com>
2025-07-04 21:41:59 +0900

    app.py

--- app.py
+++ app.py
@@ -87,7 +87,7 @@ if password == correct_pw:
         for d in dates:
             with st.expander(f"📅 {d}"):
                 for _, row in diary[diary["날짜"] == d].iterrows():
-                    st.markdown(f"**{row['감정']}**  
+                    st.markdown(f"**{row['감정']}**")  
 {row['일기']}")
     except:
         pass
